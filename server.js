035727beb60e46a49546877ae588bd8d66797d95
musembi77 <sammymusembi77@gmail.com>
2022-09-02 23:26:35 +0300

    allowing https://try-fashion-client-web.vercel.app/ to access server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http')
 const app = require('./_app.js')
 
+require('dotenv').config();
+
 const server = http.createServer(app)
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, (req,res)=>{
 	console.log(`connected at http://localhost:${port}`)
